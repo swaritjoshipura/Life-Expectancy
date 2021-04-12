1776a92c3d2c50b7a93957af48f1ffe99bb5751a
Alex <alexcmoh@gmail.com>
2021-04-12 18:04:53 -0400

    Removed some test code

--- test.py
+++ test.py
@@ -6,4 +6,4 @@ reduced_data, column_names = preprocessing.run("lifeexpecdata.csv")
 print(reduced_data)
 print(column_names)
 
-np.savetxt("foo.csv", reduced_data, delimiter=",")
+#np.savetxt("foo.csv", reduced_data, delimiter=",")
