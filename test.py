3e68ea40706dba4bdf158994b04c317040aa324d
Pranav Hariharane <pharihar@purdue.edu>
2021-04-14 00:43:09 -0700

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import preprocessing
-import numpy as np
-
-reduced_data, column_names = preprocessing.run("lifeexpecdata.csv")
-
-print(reduced_data)
-print(column_names)
-
-#np.savetxt("foo.csv", reduced_data, delimiter=",")
